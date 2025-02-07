e1ee96b36b1f224255527d4e31c30343bea5d0aa
gen <kyliki@yandex.ru>
2025-02-07 23:14:58 +0300

    init with styles

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Test Page</h1>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam fugiat minus mollitia
+        nesciunt optio vitae voluptates. Assumenda facilis id illo illum laborum laudantium
+        molestias, numquam quas quisquam sequi voluptate.
+      </div>
+    </main>
+  );
+}
