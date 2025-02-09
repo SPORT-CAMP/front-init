41a21398ffbc9d83fcb4641621268dde50c8ee48
gen <kyliki@yandex.ru>
2025-02-10 00:57:28 +0300

    hero layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import pageStyles from './page.module.scss';
+import wrapperStyles from '../styles/utils/wrapper-centered.module.scss';
+import TeamIcon from '../assets/img/icons/team.svg';
+import DifficultiesIcon from '../assets/img/icons/difficulties.svg';
+import VictoryIcon from '../assets/img/icons/victory.svg';
+import MultiStarIcon from '../assets/img/icons/multi-star.svg';
+
 export default function Home() {
   return (
     <main>
-      <h1>Сборы в Питере</h1>
+      <div className={pageStyles.hero}>
+        <div className={`section ${pageStyles.section}`}>
+          <div className={pageStyles.wrapperMultiStar}>
+            <MultiStarIcon />
+          </div>
+          <h1 className={`h1 ${pageStyles.pageTitle}`}>
+            Сборы <br /> по <span className={pageStyles.highlight}>фигурному катанию</span>{' '}
+            на&nbsp;коньках&nbsp;в&nbsp;Питере
+          </h1>
+          <div className={wrapperStyles.centered}>
+            <ul className={pageStyles.list}>
+              <li className={pageStyles.listItem}>
+                <TeamIcon />
+                Тренируйся с командой профессионалов.
+              </li>
+              <li className={pageStyles.listItem}>
+                <DifficultiesIcon />
+                Преодолевай сложности и преграды на пути к вершинам мастерства.
+              </li>
+              <li className={pageStyles.listItem}>
+                <VictoryIcon />
+                Становись лучшим и побеждай.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
